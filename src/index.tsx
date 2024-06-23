844de030d32ccea2eb7e921c18c9121c599259d5
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-06-23 13:59:06 +0200

    focus on input on page load with refs,

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-import UserSearch from './state/UserSearch';
+import UserSearch from './refs/UserSearch';
+import React from 'react';
 
-const App = () => {
+const App: React.FC = () => {
     return <div>
         <UserSearch />
     </div>
 };
 
-// const container: any = document.querySelector('#root');
-// const root: any = createRoot(container);
-// root.render(App />);
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+const container: any = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
+// ReactDOM.render(
+//     <App />,
+//     document.querySelector('#root')
+// );
